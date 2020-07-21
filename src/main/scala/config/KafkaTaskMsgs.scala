49f23acdcd206a3046863724f6cf1009e0b6b2c4
Chisanan232 <chi10211201@cycu.org.tw>
2020-07-21 22:24:36 +0800

    Add 2 new messages and modify 2 messages parameters

--- src/main/scala/config/KafkaTaskMsgs.scala
+++ src/main/scala/config/KafkaTaskMsgs.scala
@@ -10,11 +10,13 @@ trait TaskMag {
 
 case class DataExist(content: String) extends TaskMag
 case class ProcessingAsk(content: String) extends TaskMag
-case class DataComing(content: String) extends TaskMag
+case class DataComing(content: String, taskNumber: Int) extends TaskMag
 case class DataRequire(content: String) extends TaskMag
 case class WaitData(content: String, taskNumber: Int) extends TaskMag
-case class CallKafkaProducer(content: String, data: Any) extends TaskMag
+case class CallKafkaProducer(content: String, data: Any, taskNumber: Int) extends TaskMag
 case class CallKafkaConsumer(content: String, taskNumber: Int) extends TaskMag
 case class GenerateData(content: String, data: Any) extends TaskMag
 case class ConsumeData(content: String, consumerID: Int, taskNumber: Int) extends TaskMag
+case class ProduceDone(content: String) extends TaskMag
+case class ConsumeDone(content: String) extends TaskMag
 case class FinishTask(content: String) extends TaskMag
